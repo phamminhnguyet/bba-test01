5ff2a8409fbc44f30b3f5fe70a5673de621a0e93
nguyet.pham <phamminhnguyet0508@gmail.com>
2025-11-01 19:09:19 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,16 @@
+function findPairsDivisibleBy17() {
+    let count = 0;
+
+    for (let i = 1; i <= 100; i++) {
+        for (let j = i + 1; j <= 100; j++) {
+            if ((i + j) % 17 === 0) {
+                console.log("(" + i + ", " + j + ") " + (i+j));
+                count++;
+            }
+        }
+    }
+    console.log(`\nTổng cộng: ${count} cặp`);
+}
+
+// Test
+findPairsDivisibleBy17();
